f80ec880e4a917cfe4e28f1f27388c8292fd1c86
Maxwell Griffith <maxwellgriffith345@gmail.com>
2020-12-31 13:34:51 -0600

    set loops for all years, possible issue with multiple race names

--- F1_Scraper/f1_scraper.py
+++ F1_Scraper/f1_scraper.py
@@ -22,12 +22,12 @@ def get_cat_links(soup):
     cat_soup = soup.select('div.resultsarchive-filter-wrap')[1]
     return(scrape_links(cat_soup))
 
-#NEED TO CHANGE THE NAME OF THIS FUNCTION
+#NEED TO RENAME
 def get_race_links(soup):
     race_soup = soup.select('div.resultsarchive-filter-wrap')[2]
     return(scrape_links(race_soup))
 
-def scrape_table(soup): #slow 
+def scrape_table(soup):
     table = []
     res_table = soup.find(class_ = 'resultsarchive-table')
     table_body = res_table.find('tbody')
@@ -52,11 +52,10 @@ def write_csv(filename, table):
         outwriter.writerow(row)
 
 def get_file_name(url,path): 
-    #gotta be a faster way to get this info, 
     #how to deal with duplicate race names?
     new_url = url
     stripurl = new_url.lstrip('https://www.formula1.com/en/results.html/').rstrip('.html')
-    spliturl = stripurl.split('/') #5sections
+    spliturl = stripurl.split('/')
     if(len(spliturl)) < 3:
         year_race = spliturl[0]+'all'
     else:
@@ -79,30 +78,24 @@ driver_path = '/Users/maxwellgriffith/Documents/MyProjects/Python_Projects/F1_Sc
 race_path = '/Users/maxwellgriffith/Documents/MyProjects/Python_Projects/F1_Scraper/raceresults/'
 main_url = 'https://www.formula1.com/en/results.html'
 main_soup = get_soup(main_url)
-
-#GET DRIVER RESULTS FOR YEAR
 year_links = get_year_links(main_soup) #get all year links
-yearlink2019 = year_links[1] #get link for 2019
 
-driverlinks2019 = get_driver_linklist(yearlink2019)
+#GET ALL DRIVER RESULTS BY YEAR
+for year in year_links:
+    driverlinks = get_driver_linklist(year)
+    for driverlink in driverlinks:
+        filename = get_file_name(driverlink, driver_path)
+        driver_soup = get_soup(driverlink)
+        driver_table = scrape_table(driver_soup)
+        write_csv(filename,driver_table)
 
-for driverlink in driverlinks2019:
-    filename = get_file_name(driverlink, driver_path)
-    driver_soup = get_soup(driverlink)
-    driver_table = scrape_table(driver_soup)
-    write_csv(filename,driver_table)
+#GET ALL RACE RESULTS BY YEAR
+for year in year_links:
+    year_soup = get_soup(year)
+    racelinks = get_race_links(year_soup)
+    for link in racelinks:
+        file_name = get_file_name(link,race_path)
+        race_soup = get_soup(link)
+        race_table = scrape_table(race_soup)
+        write_csv(file_name,race_table)
 
-"""
-#GET RACE RESULTS FOR YEAR
-
-yearlinks = get_year_links(soup)
-racelinks_2019 = get_race_links(soup)
-
-hungary2020 = scrape_table(get_soup(racelinks_2019[0]))
-
-for link in racelinks_2019:
-    file_name = get_file_name(link,path)
-    race_soup = get_soup(link)
-    race_table = scrape_table(race_soup)
-    write_csv(file_name,race_table)
-"""
